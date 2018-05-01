2e36616e19e83a31d3ac1ed8113002533e3d503d
Ruben-Boots <ruben_boots@hotmail.com>
2018-05-01 14:21:05 +0200

    Klaar?

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,18 +1,19 @@
-import java.util.Scanner;
-import java.util.Arrays;
+//import java.util.Scanner;
+import java.util.*;
 
 public class Hangman {
 	Scanner input = new Scanner(System.in);
 	
 	public static void main(String[] args) {
+		System.out.println("Welkom bij Guillotineman (Hangman)");
 		Hangman a = new Hangman();
 		Woord woord = new Woord(a.vraagWoord());
 		System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 		System.out.println("Raad het woord dat hieronder moet verschijnen.");
-		woord.printGeraden();
+		woord.printMijnWoord();
 		
 		for (int i = 8; i>0; i--) {
-			System.out.println("Je hebt nog " + i + " poging(en)");
+			System.out.println("Het mes valt over " + i + " poging(en)");
 			String raad = a.vraagLetter();
 			if (raad.length() == 1) {
 				woord.raden(raad.charAt(0));
@@ -20,14 +21,20 @@ public class Hangman {
 				woord.gok(raad);
 			}
 			
-			if (Arrays.equals(woord.letters, woord.lettersGeraden)) {
-				System.out.println("Goedzo, je hebt het geraden!");
+			if (woord.check()) {
+				System.out.println("Goedzo, je hebt het geraden! Je bent veilig");
 				System.out.print("Het woord was inderdaad: ");
-				woord.printGeraden();
+				woord.printMijnWoord();
 				break;
+			} else {
+			woord.printMijnWoord();
+			woord.printLettersGeraden();
 			}
-			woord.printGeraden();
 		}
+		if (!woord.check()) {
+			System.out.println("Helaas, je hebt het niet geraden, dus je bent onthoofd. Het woord was: " + woord.woordKlein);
+		}
+		
 		
 		
 	}
@@ -39,56 +46,71 @@ public class Hangman {
 	}
 	
 	public String vraagLetter() {
-		System.out.println("Raad een letter.");
+		System.out.println("Raad een letter of het woord.");
 		String letter = input.next();
 		return letter;
 	}
 }
 
 class Woord {
-	char[] letters;
-	char[] lettersGeraden;
+	String woordKlein;
+	char[] mijnWoord;
+	ArrayList<Character> lettersGeraden = new ArrayList<Character>();
 	
 	Woord (String woord) {
-		String woordKlein = woord.toLowerCase();
-		letters = new char[woord.length()];
-		lettersGeraden = new char[woord.length()];
+		woordKlein = woord.toLowerCase();
+		mijnWoord = new char[woord.length()];
 		for (int i = 0; i < woord.length(); i++) {
-			letters[i] = woordKlein.charAt(i);
-			lettersGeraden[i] = '_';
+			mijnWoord[i] = '_';
 		}
 	}
 	
 	public void raden(char letter) {
-		for (int i=0; i<lettersGeraden.length; i++) {
-			if (letters[i]==letter) {
-				lettersGeraden[i] = letters[i];
-			}
+		boolean ja = false;
+		for (int i=0; i<mijnWoord.length; i++) {
+			if (woordKlein.charAt(i) == letter) {
+				mijnWoord[i] = woordKlein.charAt(i);
+				ja = true;
+			} 
+		}
+		if (!ja) {
+			lettersGeraden.add(letter);
 		}
 	}
 	
-	public void printGeraden() {
+	public void printMijnWoord() {
 		System.out.println();
-		for (int i=0; i<lettersGeraden.length; i++) {
-			System.out.print(lettersGeraden[i] + " ");
+		for (int i=0; i<mijnWoord.length; i++) {
+			System.out.print(mijnWoord[i] + " ");
+		}
+		System.out.println();
+	}
+	
+	public void printLettersGeraden() {
+		System.out.println("\n");
+		System.out.println("Je hebt al fout geraden: ");
+		for (char letter : lettersGeraden) {
+			System.out.print(letter + " ");
 		}
 		System.out.println("\n");
 	}
 	
 	public void gok(String gok) {
-		if (gok.length() == letters.length) {
-			for (int i = 0; i<gok.length(); i++) {
-				if (gok.charAt(i) != letters[i]) {
-					System.out.println(gok + " is niet goed.");
-					return;
-				}
+		if (gok.equals(woordKlein)) {
+			for (int i=0; i<mijnWoord.length; i++) {
+				mijnWoord[i] = gok.charAt(i);
 			}
 		} else {
 			System.out.println(gok + " is niet goed.");
 			return;
 		}
-		for (int i=0; i<lettersGeraden.length; i++) {
-			lettersGeraden[i] = gok.charAt(i);
+	}
+	
+	public boolean check() {
+		if (woordKlein.equals(new String(mijnWoord))) {
+			return true;
+		} else {
+			return false;
 		}
 	}
 }
